6ebbde62f8367d9f426b524f895056c8deaf9fc4
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2021-04-19 18:25:43 -0700

    CuteKitty

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -1,4 +1,7 @@
 package _01_race_car;
+
+import javax.swing.JOptionPane;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 1
@@ -11,14 +14,49 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+String rb = JOptionPane.showInputDialog("What is your racecar's brand?");
+		RaceCar rc = new RaceCar(rb, 5);
 		// 2. Print the RaceCar's position in the race
-
-		// 3. Crash the RaceCar
+		rc.crash();
 		
-		// 4. If the car is damaged. Bring it in for a pit stop.
+		if (rc.damaged) {
+			rc.pit();
+		}
+		for (int i = 0; i < args.length; i++) {
+		System.out.println("Awesome race car is in" + rc.getPositionInRace());
+		String rd = JOptionPane.showInputDialog("What is your racecar's brand?");
+		}
+	
+	rc.crash();
+		
+if (rc.damaged) {
+	rc.pit();
+}
+while (true) {
+	String rd = JOptionPane.showInputDialog("Where will you drive? Down the steep hill, up the mountain, or through the swamp");
+if (rc.getPositionInRace() == 1) {
+	JOptionPane.showMessageDialog(null, "You won!");
+	return;
+}
+	else if (rd.equals("hill") || rd.equals("steep hill") || rd.equals("Down the steep hill") || rd.equals("down the steep hill")) {
+JOptionPane.showMessageDialog(null, "You doom down the hill at exteme speeds but soon hit a rock!");
+rc.crash();
+if (rc.damaged) {
+	rc.pit();
+}
+}
+else if (rd.equals("mountain") || rd.equals("Mountain") || rd.equals("Up the mountain") || rd.equals("up the mountain")) {
+JOptionPane.showMessageDialog(null, "You climb up the massive mountain but you run out of gas. You find a gas station and restraunt so you take a pit stop.");
+rc.pit();
+}
+else if (rd.equals("swamp") || rd.equals("Swamp") || rd.equals("through the swamp") || rd.equals("Through the swamp")) {
+JOptionPane.showMessageDialog(null, "You begin slowly going through the murk but then you find a shortcut. Yay!");
+rc.overtake();
+}
 
 		// 5. Help the car move into first place.
 
+
 	}
-}
+	}
+	}
---
+++ src/_02_cat/CatRunnner.java
@@ -0,0 +1,19 @@
+package _02_cat;
+
+public class CatRunnner {
+public static void main(String[] args) {
+
+	Cat purr = new Cat ("kitty");
+purr.meow();
+purr.printName();
+purr.kill();
+purr.kill();
+purr.kill();
+purr.kill();
+purr.kill();
+purr.kill();
+purr.kill();
+purr.kill();
+purr.kill();
+}
+}
